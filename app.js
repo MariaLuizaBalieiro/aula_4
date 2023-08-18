40b95a61b2adcc174dcb97797f8cb242e2ac5641
Maria Luiza <maria.luiza.costa.balieiro@gmail.com>
2023-08-18 14:48:12 -0300

    em andamento

--- app.js
+++ app.js
@@ -1,62 +1,59 @@
 /*
-* Objetivo:  
+* Objetivo: Realizar a entrada de dois valores e a operação desejada (somar, subitari multiplicar e dividir)
 Autora: Maria Luiza 
 Data: 11/08/2023
 Versão: 1.0
+
+
+touppercase()- permite converter uma string em MAIUSCULO
+tolowercase()- permite converter uma string em minusculo
+replace - permite localizar um caracter e substituir por outro
 */
 
-
+//import da biblioteca para entrda de dados 
 var readline = require('readline');
 
+//cria objeto para entrada de dados 
 var entradaDados = readline.createInterface ({
 input: process.stdin,
 output: process.stdout
 });
 
-console.log ('********************************')
+//entrada de dados 
+console.log ('*****************************************************************')
 
 entradaDados.question('Adicione o primeiro número:' , function (primeiroNumero){
-    let numero1 = primeiroNumero;
+    let numero1 = primeiroNumero.replace(',','.');
 
     entradaDados.question('Adicione o segundo número:' , function (segundoNumero){
-        let numero2 = segundoNumero;
+        let numero2 = segundoNumero.replace(',','.');
 
+        //entrada de dados referente a operção matemática
         entradaDados.question('Informe qual é a operção desejada +, -, x, %' , function(operacaoDesejada){
             let escolhasinal = operacaoDesejada;
 
-            if(numero1 == null || numero2 == null ){
-                console.log('ERRO preencha todos os dados!')
+           //validação para entrada de dados vazia e vaçidação para entrada de caracteres ao inves de números
+            if(numero1 == "" || numero2 == "" ){
+                console.log('ERRO preencha TODOS os dados!')
                 
             } else if(isNaN(numero1) || isNaN(numero2) ){
-                console.log('Insira um número válido')
-
-            }
-
-            numero1 = parseFloat (primeiroNumero)
-            numero2 = parseFloat (segundoNumero)
-
-
-            if(escolhasinal == '+'){
-                let resultado = numero1 + numero2
-                console.log('Resultado:' + resultado)
-            } else if (escolhasinal == '-'){
-                resultado = numero1 - numero2
-                console.log('Resultado:' + resultado)
-
-            }  else if (escolhasinal == 'x'){
-                resultado = numero1 * numero2
-                 console.log('Resultado ' + resultado)
-           
-            } else if (escolhasinal == '%'){
-                resultado = numero1 / numero2
-                console.log('Resultado ' + resultado)
+                console.log('É obrigatório a entrada de valores numéricos')
 
             } else{
-                console.log('ERRO preencha todos os dados!')
-            } 
-        
-
-
+                let resultado;
+                numero1 = Number (numero1)
+                numero2 = Number (numero2)
+               
+                if(operacaoDesejada == '+'){
+                    resultado = Number (numero1) + Number(numero2);
+                }else if (operacaoDesejada == '-'){
+                    resultado = Number (numero1) - Number(numero2)
+                }else if (operacaoDesejada == 'x'){
+                    resultado = numero1 * numero2
+                } else if(operacaoDesejada = '%'){
+                    resultado = numero1 / numero2
+                }
+            }
 
 
         });
